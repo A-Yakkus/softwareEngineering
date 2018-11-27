04c3bd469e7cceec1847ae99e3db78de18cf95c8
TheFlamingSloth <TheFlamingSloth@users.noreply.github.com>
2018-11-27 14:35:35 +0000

    tmdb work
    Trying to get tmdb functionality

---
+++ Workshops/w4/src/main/java/acme/data/MovieDataTMDB.java
@@ -0,0 +1,19 @@
+package acme.data;
+
+public class MovieDataTMDB {
+    public String poster_path;
+    public Boolean adult;
+    public String overview;
+    public String release_date;
+    public String original_title;
+    public String[] genre_ids;
+    public Integer id;
+    public String media_type;
+    public String original_language;
+    public String title;
+    public String backdrop_path;
+    public float popularity;
+    public int vote_count;
+    public Boolean video;
+    public float vote_average;
+}
